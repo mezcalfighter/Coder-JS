9df95115c136324d76f623662063b14980151c5d
Emanuel Camarena <emanuel.ortega@hotmail.com>
2022-10-26 20:30:39 -0500

    Clase 1 variables

---
+++ clase01/ejemplo_01.js
@@ -0,0 +1,25 @@
+// Compras de una despensa básica:
+
+/**
+* Calcular el costo total de una compra de super:
+*
+* 3kg de Tomate
+* 1/2 de frijol
+* 1/2 de cebolla
+* 1/4 de chile
+*
+* kg de tomate: 16.64
+* kg de frijol: 27
+* kg de cebolla: 18.90
+* kg de chile: 11
+*/
+
+const tomate = 16.67
+const frijol = 27
+const cebolla = 18.90
+const chile = 11
+
+const total = (tomate * 3) + (frijol / 2) + cebolla + (chile /4)
+
+console.log('Total:', total)
+
---
+++ clase01/ejercicio_01.js
@@ -0,0 +1,4 @@
+const PI = 3.1415
+let radio = prompt("Ingrese el valor del radio:")
+const total = PI * (radio**2)
+console.log(`Total: ${total}`)
